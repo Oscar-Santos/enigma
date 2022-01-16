eb0282154c1ca030cd88c1bb41c4c9d1f677b335
oscar santos <oscar303data@gmail.com>
2022-01-15 20:59:14 -0700

    Feat: create alphabet spec file

---
+++ spec/alphabet_spec.rb
@@ -0,0 +1,16 @@
+require_relative 'spec_helper'
+require './lib/alphabet'
+
+RSpec.describe "alphabet" do
+  it "exists" do
+    alphabet = Alphabet.new
+    expect(alphabet).to be_a Alphabet
+  end
+
+  it "has attributes" do
+    alphabet = Alphabet.new
+    expect(alphabet.index).to be_a Hash
+    expect(alphabet.index.count).to eq 27
+    expect(alphabet.alphabet_array).to be_a Array
+  end
+end
