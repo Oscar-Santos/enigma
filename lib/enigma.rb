f91e8d4fef4a87010a6525047bda5e13142bd7fe
oscar santos <oscar303data@gmail.com>
2022-01-16 09:16:45 -0700

    Create encrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -22,4 +22,24 @@ class Enigma
     d_offset = key.slice(3..4).to_i + date.slice(3).to_i
     [a_offset, b_offset, c_offset, d_offset]
   end
+
+  def encrypt(message, key = generate_key , date = generate_date)
+    shift = shift(key, date)
+    message_array = message.chars
+    collector = []
+    message_array.each_with_index do |letter, index|
+      if index % 4 == 0
+        
+        collector << @alphabet_array.rotate(shift[0])[letter.to_i]
+
+      elsif index % 4 == 1
+        collector << @alphabet_array.rotate(shift[1])[index]
+      elsif index % 4 == 2
+        collector << @alphabet_array.rotate(shift[2])[index]
+      elsif index % 4 == 3
+        collector << @alphabet_array.rotate(shift[3])[index]
+      end
+    end
+
+  end
 end
